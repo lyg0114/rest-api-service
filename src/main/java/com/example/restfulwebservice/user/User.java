2716db179dd52385a7116f6e79b23e6ebda8c173
yglee <yglee@ksmartpia.com>
2023-01-07 08:59:22 +0900

    add UserDaoSercie

---
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -0,0 +1,12 @@
+package com.example.restfulwebservice.user;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+  private Integer id;
+  private String name;
+  private Date joinDate;
+}
---
+++ src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -0,0 +1,37 @@
+package com.example.restfulwebservice.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class UserDaoService {
+  private static List<User> users = new ArrayList<User>();
+  private static int userConunt = 3;
+
+  static {
+    users.add(new User(1, "user1", new Date()));
+    users.add(new User(2, "user2", new Date()));
+    users.add(new User(3, "user3", new Date()));
+  }
+
+  public List<User> findAll() {
+    return users;
+  }
+
+  public User save(User user) {
+    if (user.getId() == null) {
+      user.setId(++userConunt);
+    }
+    users.add(user);
+    return user;
+  }
+
+  public User findOne(int id) {
+    for (User user : users) {
+      if (user.getId() == id) {
+        return user;
+      }
+    }
+    return null;
+  }
+}
