34d2b26a0b94eb89f8e5d48bae807d5d73bbe880
yglee <yglee@ksmartpia.com>
2023-01-07 08:15:04 +0900

    add slash

--- src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-  @GetMapping(path = "hello-world")
+  @GetMapping(path = "/hello-world")
   public String helloWorld(){
     return "Hello World";
   }
