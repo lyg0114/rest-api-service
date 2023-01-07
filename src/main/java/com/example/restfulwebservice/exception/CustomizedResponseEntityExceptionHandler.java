4d4a09fe003f1adb63a6d44c85072ca361484198
lyg0114 <whdnseowkd@naver.com>
2023-01-07 11:33:07 +0900

    add handleUserNotFoundException

--- src/main/java/com/example/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.restfulwebservice.exception;
 
+import com.example.restfulwebservice.user.UserNotFoundException;
 import java.util.Date;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +13,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 @ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
   @ExceptionHandler(Exception.class)
   public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
     ExceptionResponse exceptionResponse = new ExceptionResponse(
         new Date(), ex.getMessage(), request.getDescription(false)
     );
     return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(UserNotFoundException.class)
+  public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+    ExceptionResponse exceptionResponse = new ExceptionResponse(
+        new Date(), ex.getMessage(), request.getDescription(false)
+    );
+    return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
   }
 }
