0dd5df03d00c36c60b5cef770719093dbe69b819
lyg0114 <whdnseowkd@naver.com>
2023-01-07 11:30:09 +0900

    add UserNotFoundException

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -2,9 +2,11 @@ package com.example.restfulwebservice.user;
 import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
   private Integer id;
   private String name;
