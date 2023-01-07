b5bc6db891d228da9fc1bc503a59e592c875ae5a
lyg0114 <whdnseowkd@naver.com>
2023-01-07 11:04:08 +0900

    add createUser

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -3,6 +3,8 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +20,10 @@ public class UserController {
   @GetMapping(path = "/users/{id}")
   public User retrieveAllUser(@PathVariable int id) {
     return service.findOne(id);
+  }
+
+  @PostMapping("/users")
+  public void createUser(@RequestBody User user) {
+    User savedUser = service.save(user);
   }
 }
