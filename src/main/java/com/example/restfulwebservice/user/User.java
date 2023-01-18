8b16fb91afef8fcaa3e9090aad1665e49b8939e5
lyg0114 <whdnseowkd@naver.com>
2023-01-18 20:48:38 +0900

    add @JsonIgnoreProperties

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,6 +1,6 @@
 package com.example.restfulwebservice.user;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -10,20 +10,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.util.StringUtils;
 
+@JsonIgnoreProperties(value = {"password", "ssn"})
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+
   private Integer id;
   @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
   private String name;
   @Past
   private Date joinDate;
   private Date updateDate;
-  @JsonIgnore
   private String password;
-  @JsonIgnore
   private String ssn;
 
   public void update(User updateUser) {
