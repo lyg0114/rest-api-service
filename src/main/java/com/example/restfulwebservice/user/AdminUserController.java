247046f97822e65207cf6eb852cb78ae0d4070aa
lyg0114 <whdnseowkd@naver.com>
2023-01-18 21:58:42 +0900

    using MappingJacksonValue

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -1,7 +1,11 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminUserController {
+
   private final UserDaoService service;
 
   @GetMapping(path = "/users")
@@ -19,8 +24,16 @@ public class AdminUserController {
   }
 
   @GetMapping(path = "/users/{id}")
-  public User retrieveAllUser(@PathVariable int id) {
-    return service.findOne(id);
+  public MappingJacksonValue retrieveAllUser(@PathVariable int id) {
+    User user = service.findOne(id);
+    String[] filterStr = {"id", "name", "joinDate", "password", "ssn"};
+    SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+        .filterOutAllExcept(filterStr);
+    FilterProvider filters = new SimpleFilterProvider()
+        .addFilter("UserInfo", filter);
+    MappingJacksonValue mapping = new MappingJacksonValue(user);
+    mapping.setFilters(filters);
+    return mapping;
   }
 
 }
