5f69ab0fa1d07e78a167c2f659176f7c0991d945
lyg0114 <whdnseowkd@naver.com>
2023-01-23 09:32:49 +0900

    add Swagger Config

---
+++ src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.example.restfulwebservice.config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+  private static final Contact DEFAULT_CONTACT
+      = new Contact("YGLEE", "http://www.yglee.com", "younggyo0114@gmail.com");
+
+  private static final ApiInfo DEFAULT_API_INFO
+      = new ApiInfo("Awesom API Title", "My User management REST API service", "1.0", "urn:tos",
+      DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/liceneces/LICENSE-2.0",
+      new ArrayList<>());
+
+  private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMERS
+      = new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(DEFAULT_API_INFO)
+        .produces(DEFAULT_PRODUCES_AND_CONSUMERS)
+        .consumes(DEFAULT_PRODUCES_AND_CONSUMERS)
+        ;
+  }
+}
